9050ac1bfed72dddef3f5befc692e5d33db79853
Antonio Rosales Martínez <antoniorosalesmartinez90@gmail.com>
2025-01-10 08:16:10 +0100

    feat dom

---
+++ DOM/09_01_2025_DOM/main.js
@@ -0,0 +1,8 @@
+//NODOS
+const title = document.querySelector("h1");
+const subtitulo = document.querySelector("#subtitulo");
+const parrafo = document.querySelector(".parrafo");
+const spans = document.querySelectorAll(".numero");
+
+title.innerText = "Métodos de DOM";
+console.log(title.innerText);
